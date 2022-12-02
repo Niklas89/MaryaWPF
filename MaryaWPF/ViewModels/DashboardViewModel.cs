21497d392639a659ab7289091d081c2caa8357d1
Niklas <niklas_edelstam@hotmail.com>
2022-12-02 12:18:41 +0100

    Add dialog box and check permissions

--- MaryaWPF/ViewModels/DashboardViewModel.cs
+++ MaryaWPF/ViewModels/DashboardViewModel.cs
@@ -5,9 +5,11 @@ using MaryaWPF.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MaryaWPF.ViewModels
 {
@@ -15,11 +17,15 @@ namespace MaryaWPF.ViewModels
     {
         IBookingEndpoint _bookingEndpoint;
         IMapper _mapper;
+        private readonly StatusInfoViewModel _status;
+        private readonly IWindowManager _window;
 
-        public DashboardViewModel(IBookingEndpoint bookingEndpoint, IMapper mapper)
+        public DashboardViewModel(IBookingEndpoint bookingEndpoint, IMapper mapper, StatusInfoViewModel status, IWindowManager window)
         {
             _bookingEndpoint= bookingEndpoint;
             _mapper= mapper;
+            _status = status;
+            _window = window;
         }
 
         // When the page is loaded then we'll call OnViewLoaded
@@ -27,7 +33,39 @@ namespace MaryaWPF.ViewModels
         protected override async void OnViewLoaded(object view) 
         {
             base.OnViewLoaded(view);
-            await LoadBookings();
+            try
+            {
+                await LoadBookings();
+            }
+            catch (Exception ex)
+            {
+                dynamic settings = new ExpandoObject();
+                settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                settings.ResizeMode = ResizeMode.NoResize;
+                settings.Title = "Erreur Système";
+
+                // Get a new ViewModel each time we call ShowDialogAsync(),
+                // have multiple copies of StatusInfoViewModel inside the same class
+                var info = IoC.Get<StatusInfoViewModel>();
+
+                if(ex.Message == "Forbidden")
+                {
+                    _status.UpdateMessage("Accès refusé", "Vous n'avez pas l'autorisation de voir les réservations sur l'application bureautique.");
+                    await _window.ShowDialogAsync(_status, null, settings);
+                } else
+                {
+                    _status.UpdateMessage("Fatal Exception", ex.Message);
+                    await _window.ShowDialogAsync(_status, null, settings);
+                }
+                
+
+                /* second message to show:
+                _status.UpdateMessage("Accès refusé", "Vous n'avez pas l'autorisation de vous connecter sur l'application bureautique.");
+                await _window.ShowDialogAsync(_status, null, settings);
+                */
+
+                await TryCloseAsync();
+            }
         }
 
         private async Task LoadBookings()
