211067db2be8ab376f4433172da6c2b6369cc19a
Niklas <niklas_edelstam@hotmail.com>
2022-12-05 12:07:25 +0100

    navigation menu design changes

---
+++ MaryaWPF/Models/MenuDisplayModel.cs
@@ -0,0 +1,24 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaryaWPF.Models;
+
+public class MenuDisplayModel 
+{
+    public string? Title { get; set; }
+    public PackIconKind SelectedIcon { get; set; }
+    public PackIconKind UnselectedIcon { get; set; }
+    private object? _notification = null;
+
+    public object? Notification
+    {
+        get { return _notification; }
+       // set { }
+        // set { SetProperty(ref _notification, value); }
+    }
+
+}
